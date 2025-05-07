780c6ea7007cb0d7902e7748764ed045381e0262
glynn315 <94496602+glynn315@users.noreply.github.com>
2025-05-07 12:53:21 +0800

    Implement tailwind and Navigation

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
-
-export const routes: Routes = [];
+import {HomeComponent} from './views/home/home.component';
+import { MembersComponent } from './views/members/members.component';
+import { LogsComponent } from './views/logs/logs.component';
+export const routes: Routes = [
+    {
+        path:'home',
+        component: HomeComponent
+    },
+    {
+        path:'members',
+        component: MembersComponent
+    },
+    {
+        path:'logs',
+        component: LogsComponent
+    }
+];
