837ef0996cebe47c066e2adfd3fe9f8733218429
glynn315 <94496602+glynn315@users.noreply.github.com>
2025-05-19 21:44:36 +0800

    Implement Logs Adding and Updating Time

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
 import { NavigationComponent } from "./shared/navigation/navigation.component";
+import { HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NavigationComponent, RouterLink],
+  imports: [RouterOutlet, NavigationComponent, RouterLink, HttpClientModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ src/app/models/logs.model.ts
@@ -0,0 +1,12 @@
+export interface Logs {
+    id?: string;
+    firstName : string;
+    lastName : string;
+    address : string;
+    contact : number;
+    hubType : string;
+    datetransaction : Date;
+    timeIn? : string;
+    timeOut? : string;
+    
+}
